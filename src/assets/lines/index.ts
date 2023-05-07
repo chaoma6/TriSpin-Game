e74cf6602ca8aefeb7e50b0fc67586ed2f4042c9
Chao Ma <machaosydney@gmail.com>
2023-05-07 20:15:49 +1000

    created index for lines

---
+++ src/assets/lines/index.ts
@@ -0,0 +1,13 @@
+import firstLine from "./firstLine.png";
+import secondLine from "./secondLine.png";
+import thirdLine from "./thirdLine.png";
+import fourthLine from "./fourthLine.png";
+import fifthLine from "./fifthLine.png";
+
+export default {
+  firstLine,
+  secondLine,
+  thirdLine,
+  fourthLine,
+  fifthLine,
+};
