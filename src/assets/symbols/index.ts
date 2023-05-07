3d7f3a2e97a9e0b20f98b21485d38049f204f8f0
Chao Ma <machaosydney@gmail.com>
2023-05-07 19:31:23 +1000

    added symbols to assets with a index

---
+++ src/assets/symbols/index.ts
@@ -0,0 +1,15 @@
+import banana from './banana.png';
+import berries from './berries.png';
+import crown from './crown.png';
+import diamond from './diamond.png';
+import hat from './hat.png';
+import plum from './plum.png';
+
+export default {
+	banana,
+	berries,
+	crown,
+	diamond,
+	hat,
+	plum,
+};
