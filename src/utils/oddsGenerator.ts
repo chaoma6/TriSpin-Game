a4ec531784a4a29dfe731fabc22ef4d9120b3294
Chao Ma <machaosydney@gmail.com>
2023-05-17 21:30:46 +1000

    add comments to the code - utils

--- src/utils/oddsGenerator.ts
+++ src/utils/oddsGenerator.ts
@@ -1,4 +1,12 @@
 import { winningOutcome, loosingOutcome } from '../resultStore/store';
+/* 
+oddsGenerator 函数生成一个介于 0 和 3000 之间的随机数（winOdd），用于确定获胜的概率。
+变量 playerWin、loosingOutcomeIndex、winningOutcomeIndex 和 outcome 被初始化为默认值。
+代码然后检查 winOdd 的值来确定结果：
+如果 winOdd 小于或等于 500，或大于 1000，则表示玩家失败，并随机选择一个失败结果的索引。
+如果 winOdd 落在 501-700、701-800、801-900、901-950、951-980 或 981-1000 的范围内，则表示玩家获胜，并使用 getRandomOutcome 函数随机选择具有相应支付的结果。
+最后，函数返回一个包含游戏结果的对象，包括玩家是否获胜（playerWin）、失败结果的索引（loosingOutcomeIndex）、获胜结果的索引（winningOutcomeIndex）以及选定的结果对象（outcome）
+*/
 
 interface Outcome {
 	combination: string[][];
@@ -13,8 +21,22 @@ interface OddsGeneratorResult {
 	outcome: Outcome;
 }
 
+// Function to generate a random index within the range of the given array length
+const generateRandomIndex = (array: any[]): number => {
+	return Math.trunc(Math.random() * array.length);
+};
+
+// Function to retrieve a random outcome with the specified payout from the given array of outcomes
+const getRandomOutcome = (outcomes: Outcome[], payout: number): Outcome => {
+	const filteredOutcomes = outcomes.filter((item: Outcome) => item.payout === payout);
+	const index = generateRandomIndex(filteredOutcomes);
+	return filteredOutcomes[index];
+};
 
 const oddsGenerator = (): OddsGeneratorResult => {
+	// Generate a random number between 0 and 3000 to determine the winning odds
+	const winOdd = Math.trunc(Math.random() * 3000);
+
 	let playerWin = false;
 	let loosingOutcomeIndex = 0;
 	let winningOutcomeIndex = 0;
@@ -24,52 +46,51 @@ const oddsGenerator = (): OddsGeneratorResult => {
 		payout: 0,
 	};
 
-	const winOdd = Math.trunc(Math.random() * 3000);
-
 	if (winOdd <= 500 || winOdd > 1000) {
+		// If the winOdd falls outside the range of 501-1000, player loses
 		playerWin = false;
-		loosingOutcomeIndex = Math.trunc(Math.random() * loosingOutcome.length);
+		loosingOutcomeIndex = generateRandomIndex(loosingOutcome);
 	} else if (winOdd >= 501 && winOdd <= 700) {
+		// If the winOdd falls within the range of 501-700, player wins with a payout of 4
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter((item:Outcome) => item.payout === 4);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
+		outcome = getRandomOutcome(winningOutcome, 4);
 	} else if (winOdd >= 701 && winOdd <= 800) {
+		// If the winOdd falls within the range of 701-800, player wins with a payout of 8
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter((item:Outcome) => item.payout === 8);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
+		outcome = getRandomOutcome(winningOutcome, 8);
 	} else if (winOdd >= 801 && winOdd <= 900) {
+		// If the winOdd falls within the range of 801-900, player wins with a payout between 12 and 20
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter(
-			(item:Outcome) => item.payout >= 12 && item.payout <= 20
+		outcome = getRandomOutcome(
+			winningOutcome.filter(
+				(item: Outcome) => item.payout >= 12 && item.payout <= 20
+			),
+			12
 		);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
 	} else if (winOdd >= 901 && winOdd <= 950) {
+		// If the winOdd falls within the range of 901-950, player wins with a payout between 20 and 40
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter(
-			(item:Outcome) => item.payout > 20 && item.payout <= 40
+		outcome = getRandomOutcome(
+			winningOutcome.filter(
+				(item: Outcome) => item.payout > 20 && item.payout <= 40
+			),
+			20
 		);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
 	} else if (winOdd >= 951 && winOdd <= 980) {
+		// If the winOdd falls within the range of 951-980, player wins with a payout of 50
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter((item:Outcome) => item.payout === 50);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
+		outcome = getRandomOutcome(winningOutcome, 50);
 	} else if (winOdd > 981 && winOdd <= 1000) {
+		// If the winOdd falls within the range of 981-1000, player wins with a payout of 100
 		playerWin = true;
-		const winningCombinations = winningOutcome.filter((item:Outcome) => item.payout === 100);
-		winningOutcomeIndex = Math.trunc(Math.random() * winningCombinations.length);
-		outcome = winningCombinations[winningOutcomeIndex];
+		outcome = getRandomOutcome(winningOutcome, 100);
 	}
 
 	return {
-		playerWin, // 是否获胜
-		loosingOutcomeIndex, // 失败的结果索引
-		winningOutcomeIndex, // 获胜的结果索引
-		outcome, // 结果对象
+		playerWin, // Indicates whether the player has won or not
+		loosingOutcomeIndex, // Index of the losing outcome in the loosingOutcome array
+		winningOutcomeIndex, // Index of the winning outcome in the winningOutcome array
+		outcome, // The selected outcome object
 	};
 };
 
--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,10 +1,19 @@
+/*
+打乱数组顺序的函数ShuffleArray
+
+循环从最后一项开始，使用变量 i 表示当前元素的索引。循环条件是 i > 0，即到达数组的第一个元素停止循环。
+在循环中，使用 Math.random() 生成一个介于 0 到 i 之间的随机数，使用 Math.floor() 向下取整确保获得整数值。这个随机数作为随机位置的索引，用变量 j 表示。
+使用解构赋值 [array[i], array[j]] = [array[j], array[i]]，将当前元素和随机位置的元素进行交换。这样可以实现打乱数组顺序的效果。
+循环结束后，函数返回打乱后的数组 array。
+*/
+
 const shuffleArray = (array: string[]) => {
-	// 从最后一项开始循环，依次将元素与随机位置的元素交换位置
+	// Loop from the last item and swap each element with a randomly chosen element
 	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1)); // 生成 0 到 i 之间的随机数
-		[array[i], array[j]] = [array[j], array[i]]; // 交换当前元素和随机位置的元素
+		const j = Math.floor(Math.random() * (i + 1)); // Generate a random number between 0 and i
+		[array[i], array[j]] = [array[j], array[i]]; // Swap the current element with the element at the random position
 	}
-	return array; // 返回打乱后的数组
+	return array; // Return the shuffled array
 };
 
 export default shuffleArray;
